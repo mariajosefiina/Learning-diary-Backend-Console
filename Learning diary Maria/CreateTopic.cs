bc2ff672c50c1d251f59d8535b199ff48f27aedc
mariajosefina <mariatoivanen@outlook.com>
2022-06-12 21:27:49 +0300

    Added format exception catch to user input questions

--- Learning diary Maria/CreateTopic.cs
+++ Learning diary Maria/CreateTopic.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.IO;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Learning_diary_Maria
 {
@@ -45,43 +45,55 @@ namespace Learning_diary_Maria
                 Topic topic = new Topic();
 
                 //Asking user for the topic properties
-                Console.WriteLine("Enter the topic id number, e.g. 0: ");
-                topic.Id = int.Parse(Console.ReadLine());
 
-                Console.WriteLine("Enter the title of the topic: ");
-                topic.Title = Console.ReadLine();
-
-
-                Console.WriteLine("Enter the description of the topic: ");
-                topic.Description = Console.ReadLine();
-                string description = topic.Description;
-
-                Console.WriteLine("Enter the estimated time to master this topic (in hours): ");
-                topic.EstimatedTimeToMaster = int.Parse(Console.ReadLine());
-
-                Console.WriteLine("Enter the time spent (in hours) : ");
-                topic.TimeSpent = double.Parse(Console.ReadLine());
-
-                Console.WriteLine("Enter the source of the topic (e.g. a website) :");
-                topic.Source = Console.ReadLine();
-
-                Console.WriteLine("What date did you start learning this? Write the date in dd/mm/yyyy format.");
-                topic.StartLearningDay = DateTime.Parse(Console.ReadLine());
-
-                Console.WriteLine("What day have you completed the subject? Write the date in dd/mm/yyyy format.");
-                topic.CompletionDay = DateTime.Parse(Console.ReadLine());
-
-                //Checking if the studies are in progress by comparing the finishing date to today's date.
-                topic.InProgress = topic.CompletionDay.ToLocalTime() < DateTime.Now;
-                if (topic.InProgress == true)
+                try
                 {
-                    Console.WriteLine("You have completed studying the topic!");
+                    Console.WriteLine("Enter the topic id number (e.g. 0): ");
+                    topic.Id = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Enter the title of the topic: ");
+                    topic.Title = Console.ReadLine();
+
+
+                    Console.WriteLine("Enter the description of the topic: ");
+                    topic.Description = Console.ReadLine();
+                    string description = topic.Description;
+
+                    Console.WriteLine("Enter the estimated time to master this topic (in number of hours, e.g. 5): ");
+                    topic.EstimatedTimeToMaster = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Enter the time spent (in number of hours, e.g. 5) : ");
+                    topic.TimeSpent = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Enter the source of the topic (e.g. a website URL) :");
+                    topic.Source = Console.ReadLine();
+
+                    Console.WriteLine("What date did you start learning this? (Write the date in dd/mm/yyyy format).");
+                    topic.StartLearningDay = DateTime.Parse(Console.ReadLine());
+
+                    Console.WriteLine("What day have you completed the subject? (Write the date in dd/mm/yyyy format).");
+                    topic.CompletionDay = DateTime.Parse(Console.ReadLine());
+
+                    //Checking if the studies are in progress by comparing the finishing date to today's date.
+                    topic.InProgress = topic.CompletionDay.ToLocalTime() < DateTime.Now;
+                    if (topic.InProgress == true)
+                    {
+                        Console.WriteLine("You have completed studying the topic!");
+                    }
+
+                    else if (topic.InProgress == false)
+                    {
+                        Console.WriteLine("You are still in the middle of studying the topic.");
+                    }
+
                 }
 
-                else if (topic.InProgress == false)
+                catch (FormatException e)
                 {
-                    Console.WriteLine("You are still in the middle of studying the topic.");
+                    Console.WriteLine("Please write your input as instructed in parentheses." );
                 }
+
+              
 
                 //Adding the user inputted object into topicCollection, the list of objects 
                 topicCollection.Add(new Topic(topic.Id, topic.Title, topic.Description, topic.EstimatedTimeToMaster, topic.TimeSpent, topic.Source, topic.StartLearningDay, topic.CompletionDay, topic.InProgress));
