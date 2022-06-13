d8efbb026479a3ddf60644cf1897d02f6dd07934
mariajosefina <mariatoivanen@outlook.com>
2022-06-13 21:47:13 +0300

    Improved my Topic class after recapping. Still figuring out this weeks task.

--- Learning diary Maria/Program.cs
+++ Learning diary Maria/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
-using static Learning_diary_Maria.CreateTopic;
+using static Learning_diary_Maria.Topic;
+using System.Collections.Generic;
 
 namespace Learning_diary_Maria
 {
@@ -8,47 +9,164 @@ namespace Learning_diary_Maria
     {
         static void Main(string[] args)
         {
-            //PROBLEM with code = does not save the data properly.
+            //Creating a new dictionary to save objects to 
+           
+
+
             Console.WriteLine("If you'd like to create a new topic to your learning diary, write A.");
             Console.WriteLine("Or if you'd like to see the topics you have saved, write B.");
+            Console.WriteLine("Or if you'd like to search for a topic based on its topic Id, write C.");
 
             string userInput = Console.ReadLine().ToLower();
             const string a = "a";
             const string b = "b";
+            const string c = "c";
 
             switch (userInput)
             {
                 case a:
-                    Topic.AddTopic();
+                    AddTopic();
                     break;
 
                 case b:
                     PrintTopic();
                     break;
+
+                //case c:
+                //    LookUpTopic();
+                //    break;
+
+            }
+
+
+
+            static void AddTopic()
+            {
+                List<Topic> topicCollection = new List<Topic>();
+                //Asking user for the topic properties
+
+                Console.WriteLine("Enter the topic id number (e.g. 0): ");
+                    int topicId = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Enter the title of the topic: ");
+                    string title = Console.ReadLine();
+
+
+                    Console.WriteLine("Enter the description of the topic: ");
+                    string description = Console.ReadLine();
+                    
+
+                    Console.WriteLine("Enter the estimated time to master this topic (in number of hours, e.g. 5): ");
+                    int estimatedTimeToMaster = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Enter the time spent (in number of hours, e.g. 5) : ");
+                    double timeSpent = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Enter the source of the topic (e.g. a website URL) :");
+                    string source = Console.ReadLine();
+
+                    Console.WriteLine("What date did you start learning this? (Write the date in dd/mm/yyyy format).");
+                    DateTime startLearningDay = DateTime.Parse(Console.ReadLine());
+
+                    Console.WriteLine("What day have you completed the subject? (Write the date in dd/mm/yyyy format).");
+                    DateTime completionDay = DateTime.Parse(Console.ReadLine());
+
+                    //Checking if the studies are in progress by comparing the finishing date to today's date.
+                    bool inProgress = completionDay.ToLocalTime() < DateTime.Now;
+                    if (inProgress == true)
+                    {
+                        Console.WriteLine("You have completed studying the topic!");
+                    }
+
+                    else if (inProgress == false)
+                    {
+                        Console.WriteLine("You are still in the middle of studying the topic.");
+                    }
+
+                //Creating a new object of topic class
+
+                Topic topic = new Topic(topicId, title, description, estimatedTimeToMaster, timeSpent, source, startLearningDay, completionDay, inProgress);
+
+                //Adding the user inputted object into topicCollection, the new list of objects
+                topicCollection.Add(topic);
+
+
+
+
+                //Printing the user inputted object to file 
+                string path = @"C:\Users\Maria T\source\repos\Learning diary Maria\Learning diary Maria\Topic.txt";
+                if (!File.Exists(path))
+                {
+
+                    using (System.IO.StreamWriter sw = File.AppendText(path))
+                    {
+                        sw.WriteLine(topic.Id.ToString() + "\n " + topic.Title + "\n " + topic.Description + "\n " + topic.EstimatedTimeToMaster.ToString() + "\n " + topic.TimeSpent.ToString() + "\n " + topic.Source +
+                        "\n " + topic.StartLearningDay.ToString() + "\n" + topic.CompletionDay.ToString() + "\n " + topic.InProgress.ToString());
+                    }
+                    Console.WriteLine("Your topic was saved to the learning diary!");
+                }
+                else
+                {
+                    using (System.IO.StreamWriter sw = File.AppendText(path))
+                    {
+                        sw.WriteLine(topic.Id.ToString() + "\n " + topic.Title + "\n " + topic.Description + "\n " + topic.EstimatedTimeToMaster.ToString() + "\n " + topic.TimeSpent.ToString() + "\n " + topic.Source +
+                        "\n " + topic.StartLearningDay.ToString() + "\n" + topic.CompletionDay.ToString() + "\n " + topic.InProgress.ToString());
+                    }
+                    Console.WriteLine("Your topic was saved to the learning diary!");
+
+                }
+
             }
         }
 
 
-        static void PrintTopic()
-        {
 
+
+            static void PrintTopic()
             {
-                try
 
                 {
-                    string path = @"C:\Users\Maria T\source\repos\Learning diary Maria\Learning diary Maria\Topic.txt";
-                    using (var sr = new StreamReader(path))
+                    try
+
                     {
-                        Console.WriteLine(sr.ReadToEnd());
+                        string path = @"C:\Users\Maria T\source\repos\Learning diary Maria\Learning diary Maria\Topic.txt";
+                        using (var sr = new StreamReader(path))
+                        {
+                            Console.WriteLine(sr.ReadToEnd());
+                        }
+                    }
+
+                    catch (IOException e)
+                    {
+                        Console.WriteLine("The file could not be read:");
+                        Console.WriteLine(e.Message);
                     }
                 }
-
-                catch (IOException e)
-                {
-                    Console.WriteLine("The file could not be read:");
-                    Console.WriteLine(e.Message);
-                }
             }
+
+            //static void LookUpTopic()
+            //{
+
+            //    Console.WriteLine("What topic Id would you like to look for? ");
+            //     int searchId = Int32.Parse(Console.ReadLine());
+
+            //    if (topicCollection.ContainsKey(searchId))
+            //    {
+            //        Console.WriteLine("The topic was found!");
+            //       }
+
+            //      else
+            //      {
+            //           Console.WriteLine("Topic was not found! :(");
+            //       }
+
+            
+
+
+
+
         }
-    }
-}
+
+        }
+    
+
---
+++ Learning diary Maria/Topic.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace Learning_diary_Maria
+{
+    class Topic
+    {
+
+            public int Id { get; set; }
+            public string Title { get; set; }
+            public string Description { get; set; }
+            public double EstimatedTimeToMaster { get; set; }
+            public double TimeSpent { get; set; }
+            public string Source { get; set; }
+            public DateTime StartLearningDay { get; set; }
+            public DateTime CompletionDay { get; set; }
+            public bool InProgress { get; set; }
+
+
+
+        public Topic(int id, string title, string description, double estimatedTimeToMaster, double timeSpent,
+           string source, DateTime startLearningDay, DateTime completionDay, bool inProgress)
+        {
+
+            Id = id;
+            Title = title;
+            Description = description;
+            EstimatedTimeToMaster = estimatedTimeToMaster;
+            TimeSpent = timeSpent;
+            Source = source;
+            StartLearningDay = startLearningDay;
+            CompletionDay = completionDay;
+            InProgress = inProgress;
+
+
+
+        }
+
+
+    }
+}
+   
+
+
+    
